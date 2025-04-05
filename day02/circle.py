50422c1a6ee3923a524855049df8df777253c5b8
Dana Brosh <danabrosh4@gmail.com>
2025-04-05 14:03:33 +0300

    day02

---
+++ day02/circle.py
@@ -0,0 +1,8 @@
+# S=₶r²
+#c=2₶r
+import math
+circle_radius = float(input("What is the radius of the circle? "))
+area = math.pi * circle_radius ** 2
+circumference = 2 * math.pi * circle_radius
+print("The area of the circle is: ", area)
+print("The circumference of the circle is: ", circumference)
---
+++ day02/rectangle.py
@@ -0,0 +1,8 @@
+#rectangle_area = height * width
+#rectangle_perimeter = 2 * (height + width)
+rectangle_height = float(input("What is the height of the rectangle? "))
+rectangle_width = float(input("What is the width of the rectangle? "))
+rectangle_area = rectangle_height * rectangle_width
+print("The area of the rectangle is: ", rectangle_area)
+rectangle_perimeter = 2 * (rectangle_height + rectangle_width)
+print("The perimeter of the rectangle is: ", rectangle_perimeter)
